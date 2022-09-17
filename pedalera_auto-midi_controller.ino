177ff20dd025f11eba9907181bce89ff66fa055c
Roberto <jose.roberto.montanes@gmail.com>
2022-09-17 08:18:56 -0500

    prueba de menú

--- pedalera_auto-midi_controller.ino
+++ pedalera_auto-midi_controller.ino
@@ -41,19 +41,44 @@ bool longpress = false;
 bool longpress1 = false;
 bool longpressboth = false;
 
+int ledpin = 13;
+unsigned long lastsyncread = 0;
+bool sync = false;
+
+//switches del cd4066
+int sw1;
+int sw2;
+const int sw1ctrl = 9;
+const int sw2ctrl = 10;
+unsigned long lastSw1Ctrl = 0;
+unsigned long lastSw2Ctrl = 0;
+bool sw1ctrlstate;
+bool sw2ctrlstate;
+
+//menu
+int menustate = 1;
+int buttonselect;
+unsigned long prevbuttonselect = 0;
+int trackcounterselect;
+
 unsigned long currentMillis;          // Variabele to store the number of milleseconds since the Arduino has started
 
 void setup() {
   Serial.begin(9600);                 // Initialise the serial monitor
 
   pinMode(buttonPin, INPUT);          // set buttonPin as input
+//  pinMode(sw1pin, INPUT);          // set buttonPin as input
+//  pinMode(sw2pin, INPUT);          // set buttonPin as input
+pinMode(A0, INPUT);
+pinMode(A1, INPUT);
+
   Serial.println("Press button");
 
   Display.setBrightness(5);
 
 }
 
-// Function for reading the button state
+// Function for reading the button state/////////////////////////////////////////////////////////////////////7
 void readButtonState() {
 
   // If the difference in time between the previous reading is larger than intervalButton
@@ -121,8 +146,7 @@ void readButtonState() {
 
 }
 
-
-// Function for reading the button1 state
+// Function for reading the button1 state////////////////////////////////////////////////////////////////////////
 void readButton1State() {
 
   // If the difference in time between the previous reading is larger than intervalButton
@@ -177,18 +201,52 @@ void readButton1State() {
   }
 
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void swcontrl (){
+  if ((currentMillis - lastSw1Ctrl > 3000)&&(sw1ctrlstate == HIGH)){
+    digitalWrite (sw1ctrl, LOW);
+    Serial.println("sw1ctrl LOW");
+    sw1ctrlstate = false;
+    lastSw1Ctrl = millis();
+  }
+ if ((currentMillis - lastSw2Ctrl > 3000)&&(sw2ctrlstate == HIGH)){
+    digitalWrite (sw2ctrl, LOW);
+    Serial.println("sw1ctrl LOW");
+    sw2ctrlstate = false;
+    lastSw2Ctrl = millis();
+  }  
+  sw1 = digitalRead(A0);
+  sw2 = digitalRead(A1);
 
+  if (sw1 == HIGH){
+    Serial.println("A0 high");
+  }
+  if (sw2 == HIGH){
+    Serial.println("A1 high");
+  }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void sw_number (){
   if (shortpress == true){
+    digitalWrite (sw1ctrl, HIGH);
+    Serial.println("sw1ctrl HIGH");
+    lastSw1Ctrl = millis();
+    sw1ctrlstate = true;
   Display.showNumberDec(51, false, 2, 0);
   shortpress = false;
   }
-  if (shortpress1 == true){
+  
+ if (shortpress1 == true){
   Display.showNumberDec(52, false, 2, 0);
   shortpress1 = false;
   }
 
-  if (longpressboth == true){
+  if (longpressboth == true){   
+    digitalWrite (sw2ctrl, HIGH);
+    Serial.println("sw2ctrl HIGH"); 
+    sw2ctrlstate = true; 
+    lastSw2Ctrl = millis();
+    menu_1on = !menu_1on;
   Display.showNumberDec(81, false, 2, 0);
   Serial.println("longpressboth");
   longpressboth = false;
@@ -203,10 +261,57 @@ void sw_number (){
   longpress1 = false;}
 
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void syncfn() {
+  currentMillis = millis();
+  // read the input on analog pin 5:
+  int sensorValue = analogRead(A5);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  if ((sensorValue >= 50)&&(currentMillis - lastsyncread >= intervalButton)){
+    digitalWrite(ledpin, HIGH);
+  Serial.println(sensorValue);
+  lastsyncread = millis();
+}
+else{
+  digitalWrite(ledpin, LOW);
+}
+}
+/////////////////////////////menu 1/////////////////////////////////////////////////////////////////////////////
+void menu_sync (){
+  while (menu_1on == true ){
+   currentMillis = millis(); 
+   if(menustate == 1){menu_1(); accion_1();}
+   if(menustate == 2){menu_2(); accion_2();}
+   if(menustate == 3){menu_3(); accion_3();}
+    
+  }
+}
+void menu_1(){
+  if (prevbuttonselect - currentMillis > intervalButton){
+  buttonselect = digitalRead(button1pin);
+   if(buttonselect == HIGH){}
+    Serial.println("seleccionar track"):  
+    Serial.println(trackcounterselect):  
+   
+   prevbuttonselect = millis();
+}
+}
+void accion_1{
+  
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
   currentMillis = millis();    // store the current time
   readButtonState();           // read the button state
   readButton1State();
   sw_number ();
+  swcontrl ();
+  if (sync == true){
+  syncfn ();
+  if (menu_1on == true){
+  menu_sync ();
+  }
 }
