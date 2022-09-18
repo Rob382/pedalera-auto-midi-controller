4fd67e572396922a874fd0fd20e6fc880deeb1ee
Roberto <jose.roberto.montanes@gmail.com>
2022-09-18 15:58:17 -0500

    optimización de tiempo entre funciones

--- pedalera_auto-midi_controller/pedalera_auto-midi_controller.ino
+++ pedalera_auto-midi_controller/pedalera_auto-midi_controller.ino
@@ -1,18 +1,4 @@
 
-/*
-   Bas on Tech - LONG PRESS BUTTON
-   This course is part of the courses on https://arduino-tutorials.net
-
-   (c) Copyright 2019 - Bas van Dijk / Bas on Tech
-   This code and course is copyrighted. It is not allowed to use these courses commercially
-   without explicit written approval
-
-   YouTube:    https://www.youtube.com/c/BasOnTech
-   Facebook:   https://www.facebook.com/BasOnTechChannel
-   Instagram:  https://www.instagram.com/BasOnTech
-   Twitter:    https://twitter.com/BasOnTech
-
-*/
 #include <MIDI.h>
 #include "Controller.h"
 #include <EEPROM.h>
